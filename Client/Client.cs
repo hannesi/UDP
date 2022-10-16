493cb6af08a32871d01fb5539d0c06587d239c56
hannesi <60550258+hannesi@users.noreply.github.com>
2022-10-16 04:44:12 -0700

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private const int DEFAULT_PORT = 6665;
+    private const int DEFAULT_DEST_PORT = 6666;
+
+    static void Main(String[] args)
+    {
+        int port = args.Length > 0 ? int.Parse(args[0]) : DEFAULT_PORT;
+        int destPort = args.Length > 1 ? int.Parse(args[1]) : DEFAULT_DEST_PORT;
+        var udpClient = new UdpClient(port);
+        while (true)
+        {
+            string message = Console.ReadLine() ?? string.Empty;
+            if (message == "/quit") return;
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            try
+            {
+                udpClient.Send(data, data.Length, "localhost", destPort);
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    private const int DEFAULT_PORT = 6666;
+    static void Main(String[] args)
+    {
+        int portti = args.Length > 0 ? int.Parse(args[0]) : DEFAULT_PORT;
+        var udpClient = new UdpClient(portti);
+
+        while (true) {
+            var rep = new IPEndPoint(IPAddress.Any, 0);
+            Byte[] rec = udpClient.Receive(ref rep);
+            string s = Encoding.UTF8.GetString(rec);
+            Console.WriteLine(s);
+        }
+    }
+}
