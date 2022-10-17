995f2afdb3062d6316dee658a5285954a68459dd
hannesi <60550258+hannesi@users.noreply.github.com>
2022-10-17 08:39:35 -0700

    Virtual socket

--- Server/Server.cs
+++ Server/Server.cs
@@ -7,12 +7,12 @@ class Server
     private const int DEFAULT_PORT = 6666;
     static void Main(String[] args)
     {
-        int portti = args.Length > 0 ? int.Parse(args[0]) : DEFAULT_PORT;
-        var udpClient = new UdpClient(portti);
+        int port = args.Length > 0 ? int.Parse(args[0]) : DEFAULT_PORT;
+        var virtualUdpClient = new VirtualUdpClient(port);
 
         while (true) {
             var rep = new IPEndPoint(IPAddress.Any, 0);
-            Byte[] rec = udpClient.Receive(ref rep);
+            Byte[] rec = virtualUdpClient.Receive(ref rep);
             string s = Encoding.UTF8.GetString(rec);
             Console.WriteLine(s);
         }
---
+++ Server/VirtualUdpClient.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class VirtualUdpClient : UdpClient
+{
+    // paketin pudottamisen todennaikoisyys
+    private static double packetDropChance = 0.25;
+    // paketin viivastyttamisen todennakoisyys
+    private static double packetDelayChance = 0.25;
+    // viivastyneen paketin viive ms
+    private static int packetDelayMs = 1000;
+    // bittivirheen todennakoisyys
+    private static double packetBitErrorChance = 0.25;
+    private static readonly Random random = new();
+
+    public VirtualUdpClient(int portti) : base(portti) { }
+    public new byte[] Receive(ref IPEndPoint? remoteEP)
+    {
+        while (true)
+        {
+            byte[] bytes = base.Receive(ref remoteEP);
+            // pudottaminen
+            if (random.NextDouble() <= packetDropChance)
+            {
+                Console.WriteLine("Paketti pudotettu!");
+                continue;
+            }
+            // bittivirhe
+            if (random.NextDouble() <= packetBitErrorChance)
+            {
+                Console.WriteLine("Pakettiin tullut bittivirhe!");
+                int errorByteIndex = random.Next(bytes.Length - 1);
+                bytes[errorByteIndex] ^= 1;
+            }
+            // viivastyminen
+            if (random.NextDouble() <= packetDelayChance)
+            {
+                Console.WriteLine("Paketti viivästyy!");
+                Thread.Sleep(packetDelayMs);
+            }
+            return bytes;
+        }
+    }
+}
